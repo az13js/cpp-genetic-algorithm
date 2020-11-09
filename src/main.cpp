26035fd49f7498a84a5bf6f3af350e78a83cd95a
MengShaoying <mengshaoying@aliyun.com>
2020-11-09 18:32:01 +0800

    优化了一下参数

--- src/main.cpp
+++ src/main.cpp
@@ -15,12 +15,12 @@ int main()
     MainProcess mainProcess = MainProcess();
     mainProcess.setDebug(true);
     mainProcess.run(
-        400, // 种群大小
+        240, // 种群大小
         128, // 染色体长度
         100, // 最大迭代次数
-        93.0, // 停止迭代适应度
-        90, // 每次迭代保留多少个上一代的高适应度个体
-        0.1 // 变异概率
+        90.0, // 停止迭代适应度
+        22, // 每次迭代保留多少个上一代的高适应度个体
+        0.047 // 变异概率
     );
     return 0;
 }
